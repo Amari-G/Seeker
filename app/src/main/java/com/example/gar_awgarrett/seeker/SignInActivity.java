871ab973611d3cef832abddd4133a2541f2dad77
ibceriale <ibceriale0624@gmail.com>
2017-11-07 09:45:31 -0800

    linking pages

--- app/src/main/java/com/example/gar_awgarrett/seeker/SignInActivity.java
+++ app/src/main/java/com/example/gar_awgarrett/seeker/SignInActivity.java
@@ -1,13 +1,28 @@
 package com.example.gar_awgarrett.seeker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
 
 public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_sign_in);
+
+        //creates sign up button
+        Button bSIBack = findViewById(R.id.bSIBack);
+
+        //links sign up button to sign up page
+        bSIBack.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(SignInActivity.this, WelcomeActivity.class));
+            }
+        });
     }
 }
