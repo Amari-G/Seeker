319edd869c9a8924b10ac71d4672ffdf3d9a6ee5
Amari-G <amariwgarrett@gmail.com>
2017-11-08 10:31:09 -0800

    Renamed "List" references to "Quest". Additionally navigation buttons have been added to the list page and should be ready to be added to other pages.

---
+++ app/src/main/java/com/example/gar_awgarrett/seeker/QuestActivity.java
@@ -0,0 +1,31 @@
+package com.example.gar_awgarrett.seeker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+
+/**
+ * Created by gar_awgarrett on 11/8/2017.
+ */
+
+public class QuestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_quest_page);
+
+        //creates list page button in navigation bar
+        ImageButton bNBQuest = findViewById(R.id.bNBList);
+
+        //links sign up button to sign up page
+        bNBQuest.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(QuestActivity.this, QuestActivity.class));
+            }
+        });
+    }
+}
