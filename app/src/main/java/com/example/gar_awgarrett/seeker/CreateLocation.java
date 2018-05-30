b6f7466f090f005fc28199eb2de7269bf3bcfa15
ibceriale0624 <iwouldshavemylegsforyou@gmail.com>
2018-05-30 10:27:49 -0700

    Proximity error when creating locations

--- app/src/main/java/com/example/gar_awgarrett/seeker/CreateLocation.java
+++ app/src/main/java/com/example/gar_awgarrett/seeker/CreateLocation.java
@@ -2,6 +2,7 @@ package com.example.gar_awgarrett.seeker;
 
 import android.content.Intent;
 import android.os.LocaleList;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -15,13 +16,22 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import static java.lang.Double.valueOf;
+
+import android.location.Location;
+
+import android.location.LocationListener;
+import android.location.Location;
+import android.location.LocationManager;
+
 
 public class CreateLocation extends AppCompatActivity {
 
     private SavedQuestsActivity savedQuestsActivity;
     private DatabaseReference mDatabase;
     private LocaleList newLocationList;
+    FragmentManager fm = getSupportFragmentManager();
+
+    private boolean inProximity = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,23 +64,51 @@ public class CreateLocation extends AppCompatActivity {
                 Editable editableLong = longitudeInput.getText();
                 String stringLong = editableLong.toString();
                 Double longitude = Double.parseDouble(stringLong);
+                GPSTracker user = new GPSTracker(getApplicationContext());
 
-                //creates location in database by generating a new path
-                DatabaseReference newLocationPath = mDatabase.push();
+                //checks proximity of location before adding to datatbase
+                checkProximity(latitude, longitude, user);
 
-                //creates new string with value set as the locations unique id
-                String pathId = newLocationPath.getKey();
+                if(inProximity == false){
+                    ProximityError proximityError = new ProximityError();
+                    proximityError.show(fm, "ProximityError");
+                }
 
-                //creates the location object with values created above
-                Location location = new Location(pathId, name, latitude, longitude);
+                else {
 
-                //adds location object to the path created on line 59
-                mDatabase.child(pathId).setValue(location);
+                    //creates location in database by generating a new path
+                    DatabaseReference newLocationPath = mDatabase.push();
 
-                //Restarts activity with empty fields
-                startActivity(new Intent(CreateLocation.this, CreateLocation.class));
-                finish();
+                    //creates new string with value set as the locations unique id
+                    String pathId = newLocationPath.getKey();
+
+                    //creates the location object with values created above
+                    com.example.gar_awgarrett.seeker.Location location = new com.example.gar_awgarrett.seeker.Location(pathId, name, latitude, longitude);
+
+
+                    //adds location object to the path created on line 59
+                    mDatabase.child(pathId).setValue(location);
+
+                    //Restarts activity with empty fields
+                    startActivity(new Intent(CreateLocation.this, CreateLocation.class));
+                    finish();
+                }
             }
         });
+
     }
+
+
+    //if the location is within 0.1 miles, that location will not be viable
+    public boolean checkProximity(double latitude, double longitude, GPSTracker user) {
+        double userLat = user.getLocation().getLatitude();
+        double userLong = user.getLocation().getLongitude();
+        double distance = MapPage.distance(latitude, userLat, longitude, userLong, 0.0, 0.0);
+        if (distance <= 0.1) {
+            inProximity = false;
+        }
+        return inProximity;
+    }
+
+
 }
